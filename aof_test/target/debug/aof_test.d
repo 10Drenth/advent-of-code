4e5a9afa2f407c61bff7384b3cec27c49b1a02a5
10Drenth <73843609+10Drenth@users.noreply.github.com>
2022-11-29 14:41:18 +0100

    removed target dir

--- aof_test/target/debug/aof_test.d
+++
@@ -1,1 +0,0 @@
-/home/mdrenth/Documents/advent_of_code/aof_test/target/debug/aof_test: /home/mdrenth/Documents/advent_of_code/aof_test/input.txt /home/mdrenth/Documents/advent_of_code/aof_test/src/main.rs
